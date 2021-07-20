cca5dcaadb9df0cccf5d9ceef4f7ded45bbb7e89
dhreyes <2590@holbertonschool.com>
2021-07-19 23:31:12 -0400

    Initial commit of readme, header, and start of 0

---
+++ 0-binary_tree_node.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_node - create binary_tree_node
+ * @parent: pointer to parent node
+ * @value: value for new node
+ * Return: pointer to new node, else NULL
+ */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+{
+    binary_tree_t *new= malloc(sizeof(binary_tree_t));
+
+    
+}
