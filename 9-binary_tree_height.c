8e237670a0885b96e5c471be06b3528b291a4fdf
Thegermanblob <thegermanblob@gmail.com>
2021-07-22 20:05:19 +0000

    Task 9

---
+++ 9-binary_tree_height.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_height - returns the height of a given tree
+ * @tree: given tree
+ * Return: height of the tree
+ */
+
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t rlenght = 0, llenght = 0;
+
+	if (tree)
+	{
+		rlenght = binary_tree_height(tree->right);
+		llenght = binary_tree_height(tree->left);
+
+		if (rlenght > llenght)
+			return (rlenght + 1);
+		else
+			return (llenght + 1);
+	}
+	return (0);
+
