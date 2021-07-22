01e9deba7297f2e4ce4aa7e91b09964c62a0ea7b
Thegermanblob <thegermanblob@gmail.com>
2021-07-22 20:56:03 +0000

    Make betty compliant;Fix Null edge case

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,12 +1,15 @@
 #include "binary_trees.h"
 /**
- *
- *
- *
+ * binary_tree_depth - returns how deep node is in tree
+ * @tree- given node of tree
+ * Return: returns the depth or 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth = 0;
+
+	if (!tree)
+		return (0);
 
 	while (tree->parent)
 	{
