a52ce9089807a8de340144efe23b31ecc0115496
Thegermanblob <thegermanblob@gmail.com>
2021-07-22 22:43:34 +0000

    Add task 15

---
+++ 15-binary_tree_is_full.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+ * binary_tree_is_full - checks if the tree is full
+ * @tree: given tree
+ * Return: 1 if full  or 0 if NULL or not full
+ */
+
+int binary_tree_is_full(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if (tree->left && tree->right)
+		return (binary_tree_is_full(tree->left) + binary_tree_is_full(tree->right));
+	return (0);
+}
