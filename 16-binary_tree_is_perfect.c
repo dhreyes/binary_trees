fd5eb428daa863a7c712f54ec3b0448ecc364542
dhreyes <2590@holbertonschool.com>
2021-07-23 10:29:45 -0400

    Add recursion

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -29,6 +29,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
-		return (1);
+		return (binary_tree_is_perfect(tree->left) == binary_tree_is_perfect(tree->right));
 	return (0);
 }
